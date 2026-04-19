3de88d8a05901ff2201709d92f86764358d66d08
Oleh Halas <oleggalas8@gmail.com>
2026-04-19 10:36:52 +0300

    Implement Models: Race, Skill, Guild, Playermodel. Read data from players.json

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
 import init_django_orm  # noqa: F401
+import json
+from django.db import transaction
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+
+    with open("players.json", "r") as f:
+        player_data = json.load(f)
+
+    with transaction.atomic():
+        for player_name, data in player_data.items():
+
+            race_data = data["race"]
+
+            race, _ = Race.objects.get_or_create(
+                name=race_data["name"],
+                description=race_data["description"],
+            )
+
+            for skill_data in race_data["skills"]:
+
+                skill, _ = Skill.objects.get_or_create(
+                    name=skill_data["name"],
+                    bonus=skill_data["bonus"],
+                    race=race
+                )
+
+            guild = None
+            guild_data = data["guild"]
+
+            if guild_data is not None:
+
+                guild, _ = Guild.objects.get_or_create(
+                    name=guild_data["name"],
+                    description=guild_data["description"]
+                )
+
+            player_mod, _ = Player.objects.get_or_create(
+                nickname=player_name,
+                email=data["email"],
+                bio=data["bio"],
+                race=race,
+                guild=guild
+            )
 
 
 if __name__ == "__main__":

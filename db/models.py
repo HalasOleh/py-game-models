a58a159b9ca833baccfd87acc312f1e5742659dd
Oleh Halas <oleggalas8@gmail.com>
2026-04-19 11:05:54 +0300

    Improve models

--- db/models.py
+++ db/models.py
@@ -12,7 +12,7 @@ class Skill(models.Model):
 
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='skills')
 
 
 class Guild(models.Model):
@@ -26,10 +26,12 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='players')
     guild = models.ForeignKey(
         Guild,
         null=True,
         blank=True,
-        on_delete=SET_NULL)
+        on_delete=SET_NULL,
+        related_name='members'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def main() -> None:
     with transaction.atomic():
         for player_name, data in player_data.items():
 
-            race_data = data["race"]
+            race_data = data.get("race")
 
             race, _ = Race.objects.get_or_create(
                 name=race_data["name"],
-                description=race_data["description"],
+                description={"description": race_data["description"]},
             )
 
-            for skill_data in race_data["skills"]:
+            for skill_data in race_data.get("skills"):
 
                 skill, _ = Skill.objects.get_or_create(
                     name=skill_data["name"],
@@ -29,13 +29,13 @@ def main() -> None:
                 )
 
             guild = None
-            guild_data = data["guild"]
+            guild_data = data.get("guild")
 
             if guild_data is not None:
 
                 guild, _ = Guild.objects.get_or_create(
                     name=guild_data["name"],
-                    description=guild_data["description"]
+                    description={"description": guild_data["description"]}
                 )
 
             player_mod, _ = Player.objects.get_or_create(
